e4df31daeb9d08182b0fe7efd575c6418998d437
giuliom95 <giuliomartella1995@gmail.com>
2019-09-30 12:47:19 +0000

    Using QLabel + QImage to display an image

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -9,9 +9,22 @@ MainWindow::MainWindow() : QWidget() {
 	auto* layout = new QHBoxLayout();
 	layout->setContentsMargins(0,0,0,0);
 
-	auto* canvas = new Canvas();
+	QImage buffer{300, 300, QImage::Format_RGB888};
+	auto* bufferData = buffer.bits();
+	for(int i = 0; i < 300; ++i)
+		for(int j = 0; j < 300; ++j) {
+			bufferData[3*(j*300 + i) + 0] = (uchar)(256*(i / 300.f));
+			bufferData[3*(j*300 + i) + 1] = (uchar)(256*(j / 300.f));
+			bufferData[3*(j*300 + i) + 2] = 0;
+		}
 
-	layout->addWidget(canvas);
+	QLabel* imageContainer = new QLabel();
+	imageContainer->setPixmap(QPixmap::fromImage(buffer));
+
+	QScrollArea* scrollArea = new QScrollArea();
+	scrollArea->setBackgroundRole(QPalette::Dark);
+	scrollArea->setWidget(imageContainer);
 	
+	layout->addWidget(scrollArea);
 	setLayout(layout);
 }
